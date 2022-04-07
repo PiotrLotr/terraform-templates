cc79b318baa64859ca8ca5883838fa468745b440
Piotr Wrona <pwrona9@gmail.com>
2022-04-07 12:14:56 +0200

    v.02

--- python-app-infrastructure/task.tf
+++ python-app-infrastructure/task.tf
@@ -1,18 +1,9 @@
-resource "aws_ecs_task_definition" "service-task" {
-  family = "service"
-  container_definitions = jsonencode([
-    {
-      name      = "app"
-      image     = "251865263936.dkr.ecr.eu-central-1.amazonaws.com/app-dev:latest"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-  ])
-}
+# resource "aws_ecs_task_definition" "app" {
+#   family                   = "python-service-task"
+#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = 1024
+#   memory                   = 3096
+#   container_definitions    = data.cb_app.rendered
+# }
